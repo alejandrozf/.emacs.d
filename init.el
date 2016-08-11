809c8a6b0053ec5f468dbfdb354c62d002dbadfc
Alejandro Zamora <azamora@machinalis.com>
2016-08-11 14:16:44 -0300

    Cambiando de multi-web a web-mode

--- init.el
+++ init.el
@@ -138,16 +138,29 @@ Must end with a trailing slash.")
     (ido-vertical-mode 1)
     (flx-ido-mode 1)))
 
-(use-package multi-web-mode
-  :if (not noninteractive)
-  :ensure multi-web-mode
-  :config (progn
-	    (setq mweb-default-major-mode 'html-mode)
-	    (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
-			      (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
-			      (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
-	    (setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
-	    (multi-web-global-mode 1)))
+(use-package web-mode
+  :ensure web-mode
+  :config
+  (progn
+    (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+    (setq web-mode-engines-alist
+      '(("blade"  . "\\.blade\\.")
+        ("django" . "\\.html")))
+
+    (defun my-web-mode-hook ()
+      "Hooks for Web mode."
+      (setq web-mode-markup-indent-offset 2)
+      (setq web-mode-css-indent-offset 2)
+      (setq web-mode-code-indent-offset 2))
+    (add-hook 'web-mode-hook  'my-web-mode-hook)))
 
 (use-package powerline
   :if (not noninteractive)
