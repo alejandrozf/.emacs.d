80cead149151f8525b8cdc306a79ad058d4dd22f
Alejandro Zamora <Alejandro Zamora>
2016-10-09 21:01:58 -0300

    ...

--- init.el
+++ init.el
@@ -157,6 +157,7 @@ Must end with a trailing slash.")
       (setq web-mode-markup-indent-offset 2)
       (setq web-mode-css-indent-offset 2)
       (setq web-mode-code-indent-offset 2)
+      (setq web-mode-comment-style 2)
       (setq web-mode-enable-current-element-highlight t))
     (add-hook 'web-mode-hook  'my-web-mode-hook)))
 
